1dc9396f35b742cae6ce3f547365f5bbecae0bc4
pigeonsrcool2 <pigeonsrcool2@yahoo.com>
2016-04-20 08:30:25 -0700

    Meow!
    Some css updates. I think.

--- public_html/main/aboutus.php
+++ public_html/main/aboutus.php
@@ -2,13 +2,107 @@
 	$pagetitle = "About Us";
 	include ($_SERVER ["DOCUMENT_ROOT"] . '/main/header.php');
 ?>
+
 	<div class="clear"></div>
-	<div class="description"><h2>About Us</h2></div>
-	<div class="content">
-		<p> Red Rock Telecommunications is the first company in the world to combine the power of Metaswitch with the voice 
-		 and video features of Avaya.  This allows us to be the sole provider of an all IP based communication solution. Red 
-		 Rock delivers reliable high quality hosted PBX, SIP Trunking, voice to text capabilities, mobile device integration,
-		 and much more. </p>
-	</div>	
-	
+	 <div id="slider-polaroid-0" class="slider slider-polaroid polaroid no-responsive" style="height:400px;">
+            <div class="thumbs  container">
+                <div class="thumb">
+                    <img src="/assets/images/accession.png" alt="/images/slider/flexslider/001.png" />
+                    <div class="slide-content container align-right" style="background-image:url('/assets/images/accession.png');">
+                        <div class="text">
+                            <h2>With Red Rock we will make you HAPPY.</h2>
+                            <p>
+                                Quisque nec mi eu nibh aliquam elementum. Ut cursus nisl sit amet sapien dignissim at adipiscing lectus ornare. Aenean id lorem orci.
+                                Morbi lacinia nunc quis lectus condimentum rutrum.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="thumb">
+                    <img src="/images/slider/flexslider/0026-150x150.jpg" alt="/images/slider/flexslider/0026.jpg" />
+                </div>
+
+                <div class="thumb">
+                    <img src="/images/slider/flexslider/003-150x150.png" alt="/images/slider/flexslider/003.png" />
+                    <div class="slide-content container align-right" style="background-image:url('/assets/images/accessionman.png');">
+                        <div class="text">
+                            <h2>Be nice. Be original.</h2>
+                            <p>
+                                Quisque nec mi eu nibh aliquam elementum. Ut cursus nisl sit amet sapien dignissim at adipiscing lectus ornare. Aenean id lorem orci.
+                                Morbi lacinia nunc quis lectus condimentum rutrum.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="thumb">
+                    <img src="/images/slider/flexslider/0043-150x150.jpg" alt="/images/slider/flexslider/0043.jpg" />
+                    <div class="slide-content container align-right full" style="background-image:url('/assets/images/accessionphone.png');">
+                        <div class="container">
+                            <div class="text">
+                                <h2>
+                                    <span style="color: #0c243d;">Need a Communications</span>
+                                    <span style="color: #919303;">solution?</span>
+                                </h2>
+
+                                <p>
+                                    <span style="color: #434f5b;">Come meet RedRock</span>
+                                    <br />
+                                    <span style="color: #8b8005;">Flexible, versatile, and impeccable customer service</span>
+                                </p>
+
+                                <p>
+                                    <span style="color: #434f5b;">
+                                    A complete solution for your large or small business<br />
+                                    </span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="thumb">
+                    <img src="/images/slider/flexslider/0052-150x150.jpg" alt="/images/slider/flexslider/0052.jpg" />
+                    <div class="slide-content container align-right full" style="background-image:url('/assets/images/accessionphone.png');">
+                        <div class="container"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <script type="text/javascript">
+            jQuery(document).ready(function($){
+                $('#slider-polaroid-0').polaroid({
+                    animation: '',
+                    pause: 8000,
+                    animationSpeed: 800			    });
+            });
+        </script>
+
+        <div class="mobile-slider">
+            <div class="slider fixed-image container">
+                <img src="/images/slider/flexslider/fixed-polaroid.jpg" alt="" />
+            </div>
+        </div>
+	<div id="primary" class="sidebar-no">
+	<div class="container group">
+	<div class="row">
+	<div id="content-page" class="span 12 content group">
+	<div id="post-302" class="post-302 page type-page status-publish hentry group">
+	<h2> How Can I Unify my Communications? </h2>
+	<div class="three-fourth">
+		<p>
+			Red Rock Telecommunications is a Phoenix based company focused on providing quality hosted communications solutions.
+		</p>
+	</div>
+	<div class= "two-third">
+		<p>
+	</p>
+	</div>
+	</div>
+	</div>
+	</div>
+	</div>
+	</div>
 <?php include $root . '/main/footer.php'?>
