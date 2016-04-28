374b820cecb70013392c4beefe7a5bfca0d08a93
Cake <jbowdish@email.arizona.edu>
2016-04-27 23:09:01 -0700

    Some safety stuff before it GOES LIVE AND STUFF

--- public_html/orders/OrderConfirm.php
+++ public_html/orders/OrderConfirm.php
@@ -1,10 +1,12 @@
 <?php
 $root = $_SERVER ["DOCUMENT_ROOT"];
+include_once ($_SERVER ["DOCUMENT_ROOT"] . '/portal/portalheader.php');
 include_once $root . '/classes/DBUtils.php';
 
 if (empty ( $_POST )) {
 	echo "Something went wrong.";
 }
+
 
 $dbutils = new DBUtils ();
 $conn = $dbutils->getDBConnection ();
@@ -26,30 +28,30 @@ if (mysqli_query ( $conn, $customerInsertString )) {
 		echo '<a href= "/portal/portal.php">Return to portal</a>';
 		// echo "<script> window.setTimeout(location.href = \"" . $root . "/portal/portal.php" . "\",5000) </script>";
 	} else {
-		echo "Error inserting Order information: " . mysqli_error ( $conn );
+		//echo "Error inserting Order information: " . mysqli_error ( $conn );
 	}
 } else {
-	echo "Error: " . $customerInsertString . "<br>" . mysqli_error ( $conn );
+	//echo "Error: " . $customerInsertString . "<br>" . mysqli_error ( $conn );
 }
 
 $conn->close ();
 
 function generateCustomerInsertString() {
 	$sql = 'INSERT INTO Customers (End_User_Name, Cust_Telephone, Address_1, Address_2, City, State, Zip, Emerg_Address_1, Emerg_Address_2, Emerg_City, Emerg_State, Emerg_Zip, Emerg_Phone,Customer_Time_Zone) VALUES(';
-	$sql = $sql . "'" . $_POST["endusername"] . "',";
-	$sql = $sql . "'" . $_POST["address1"] ."',";
-	$sql = $sql . "'" . $_POST["address2"] ."',";
-	$sql = $sql . "'" . $_POST["city"] ."',";
-	$sql = $sql . "'" . $_POST["state"] ."',";
-	$sql = $sql . "'" . $_POST["zipcode"] ."',";
-	$sql = $sql . "'" . $_POST["cmtelephone"] . "',";
-	$sql = $sql . "'" . $_POST["emergaddress1"] ."',";
-	$sql = $sql . "'" . $_POST["emergaddress2"] ."',";
-	$sql = $sql . "'" . $_POST["emergcity"] ."',";
-	$sql = $sql . "'" . $_POST["emergstate"] ."',";
-	$sql = $sql . "'" . $_POST["emergzipcode"] ."',";
-	$sql = $sql . "'" . $_POST["emergphonenumber"] ."',";
-	$sql = $sql . "'" . $_POST["customertimezone"] ."')";
+	$sql = $sql . "'" . test_input($_POST["endusername"]) . "',";
+	$sql = $sql . "'" . test_input($_POST["address1"]) ."',";
+	$sql = $sql . "'" . test_input($_POST["address2"]) ."',";
+	$sql = $sql . "'" . test_input($_POST["city"]) ."',";
+	$sql = $sql . "'" . test_input($_POST["state"]) ."',";
+	$sql = $sql . "'" . test_input($_POST["zipcode"]) ."',";
+	$sql = $sql . "'" . test_input($_POST["cmtelephone"]) . "',";
+	$sql = $sql . "'" . test_input($_POST["emergaddress1"]) ."',";
+	$sql = $sql . "'" . test_input($_POST["emergaddress2"]) ."',";
+	$sql = $sql . "'" . test_input($_POST["emergcity"]) ."',";
+	$sql = $sql . "'" . test_input($_POST["emergstate"]) ."',";
+	$sql = $sql . "'" . test_input($_POST["emergzipcode"]) ."',";
+	$sql = $sql . "'" . test_input($_POST["emergphonenumber"]) ."',";
+	$sql = $sql . "'" . test_input($_POST["customertimezone"]) ."')";
 	
 	return $sql;
 }
@@ -95,30 +97,30 @@ function sendOrderAlertEmail($orderNumber){
 	Order Number ' . $orderNumber . '
 	Details: 
 		<br><br>' .
-	'Reseller Name: ' .	$_POST["resellername"] . '<br>' .
-	'Telephone Number: ' .	$_POST["telephonenumber"] . '<br>' .
-	'Email Address: ' . 	$_POST["emailaddress"] . '<br>' .
-	'Reseller Contact Name: ' . 	$_POST["resellercn"] . '<br>' .
-	'Salesrep: ' . 	$_POST["salesrep"] . '<br>' .
-	'Reseller Reference ID: ' . 	$_POST["resellerrefid"] . '<br>' .
-	'Requested built by date: ' . 	$_POST["requestedbuilt"] . '<br>' .
-	'Requested inservice by date: ' . 	$_POST["requestedinservice"] . '<br>' .
-	'Or sooner: ' . 	$_POST["orsooner"] . '<br>' .
-	'Add to existing customer: ' . 	$_POST["addtoexistingcustomer"] . '<br>' .
-	'Emergency provisioning required: ' . 	$_POST["emergprovisionrequired"] . '<br>' .
-	'End user name: ' . 	$_POST["endusername"] . '<br><br>' .
+	'Reseller Name: ' .	test_input($_POST["resellername"]) . '<br>' .
+	'Telephone Number: ' .	test_input($_POST["telephonenumber"]) . '<br>' .
+	'Email Address: ' . 	test_input($_POST["emailaddress"]) . '<br>' .
+	'Reseller Contact Name: ' . 	test_input($_POST["resellercn"]) . '<br>' .
+	'Salesrep: ' . 	test_input($_POST["salesrep"]) . '<br>' .
+	'Reseller Reference ID: ' . 	test_input($_POST["resellerrefid"]) . '<br>' .
+	'Requested built by date: ' . 	test_input($_POST["requestedbuilt"]) . '<br>' .
+	'Requested inservice by date: ' . 	test_input($_POST["requestedinservice"]) . '<br>' .
+	'Or sooner: ' . 	test_input($_POST["orsooner"]) . '<br>' .
+	'Add to existing customer: ' . 	test_input($_POST["addtoexistingcustomer"]) . '<br>' .
+	'Emergency provisioning required: ' . 	test_input($_POST["emergprovisionrequired"]) . '<br>' .
+	'End user name: ' . 	test_input($_POST["endusername"]) . '<br><br>' .
 	'<b>Emergency information</b>:<br>' .
-	'Telephone: ' . 	$_POST["cmtelephone"] . '<br>' .
-	'Address 1: ' . 	$_POST["emergaddress1"] . '<br>' .
-	'Address 2: ' . 	$_POST["emergaddress2"] . '<br>' .
-	'City: ' . 	$_POST["emergcity"] . '<br>' .
-	'Emergstate: ' . 	$_POST["emergstate"] . '<br>' .
-	'Emergzipcode: ' . 	$_POST["emergzipcode"] . '<br>' .
-	'Emergphonenumber: ' . 	$_POST["emergphonenumber"] . '<br>' .
-	'Customertimezone: ' . 	$_POST["customertimezone"] . '<br>' .
-	'Accountnumber: ' . 	$_POST["accountnumber"] . '<br>' .
-	'Spcode: ' . 	$_POST["spcode"] . '<br>' .
-	'Order Details:<br>' . 	$_POST["orderdetails"] . '<br>'
+	'Telephone: ' . 	test_input($_POST["cmtelephone"]) . '<br>' .
+	'Address 1: ' . 	test_input($_POST["emergaddress1"]) . '<br>' .
+	'Address 2: ' . 	test_input($_POST["emergaddress2"]) . '<br>' .
+	'City: ' . 	test_input($_POST["emergcity"]) . '<br>' .
+	'Emergstate: ' . 	test_input($_POST["emergstate"]) . '<br>' .
+	'Emergzipcode: ' . 	test_input($_POST["emergzipcode"]) . '<br>' .
+	'Emergphonenumber: ' . 	test_input($_POST["emergphonenumber"]) . '<br>' .
+	'Customertimezone: ' . 	test_input($_POST["customertimezone"]) . '<br>' .
+	'Accountnumber: ' . 	test_input($_POST["accountnumber"]) . '<br>' .
+	'Spcode: ' . 	test_input($_POST["spcode"]) . '<br>' .
+	'Order Details:<br>' . 	test_input($_POST["orderdetails"]) . '<br>'
 	;
 	
 	
@@ -128,9 +130,9 @@ function sendOrderAlertEmail($orderNumber){
 	$mail->AddAddress("ops@redrocktelecom.com");
 	
 	if($mail->Send()) {
-		echo "Message sent!";
+		//echo "Message sent!";
 	} else {
-		echo "Mailer Error: " . $mail->ErrorInfo;
+		//echo "Mailer Error: " . $mail->ErrorInfo;
 	}
 }
 
