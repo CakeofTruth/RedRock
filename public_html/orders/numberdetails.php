85bd50dced99f13b6a90686512b73db6407f8e98
pigeonsrcool2 <pigeonsrcool2@yahoo.com>
2016-08-23 16:58:51 -0700

    collapsible menu options
    Put on customer order form and numberdetails.  Customer order form is
    now an accordion style form, number details now only shows option based
    off what is chosen.  Modified css on item order form so it is more
    scalable.  added css to customerorderform.css so that all order forms
    (customer order form/ item order form/ number details) are more uniform.

--- public_html/orders/numberdetails.php
+++ public_html/orders/numberdetails.php
@@ -4,7 +4,7 @@
 <title>Input number details</title>
 <link rel='stylesheet' id='custom-css' href='/css/customerorderform.css'
 	type='text/css' media='all' />
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
 </head>
 <body>
 <?php
@@ -34,38 +34,37 @@ if ($OrderItemsResult->num_rows > 0) {
 <div>
 <div>
 <label for="porting">Will you be porting any numbers?<span class="required" style="color:red;">*</span>
-			</label> Yes<input type="radio" name="porting" id="porting-yes"
-				value="Yes"> No<input type="radio"
-				name="porting" id="porting-no" value="No">
+			</label> Yes<input type="radio" name="porting" id="yes"
+				value="2"> No<input type="radio"
+				name="porting" id="porting-no" value="1">
 				<br>
-				<div class="reveal-if-active">
+				<div class="yesport">
     			<a href="javascript:void(0);" onclick="addElement();">Add</a>
     			<a href="javascript:void(0);" onclick="removeElement();">Remove</a>
 				<div id="content"></div>
-					
-<label for= "btn"> Which number is the BTN? <span class="required" style="color:red;">*</span></label>
-<br>
 </div>
 </div>
 <label for="newnumbers">Do you need any new numbers?<span class="required" style="color:red;">*</span>
-			</label> Yes<input type="radio" name="newnumbers" id="newnumbers-yes"
-				value="Yes"> No<input type="radio"
-				name="newnumbers" id="newnumbers-no" value="No">
-				<div class="reveal-if-active">
+			</label> Yes<input type="radio" name="newnumbers" id="yes"
+				value="2"> No<input type="radio"
+				name="newnumbers" id="no" value="1">
+				<div class="yesnew">
 <label for="newnumberquantity"> How many new numbers will you need?</label> <input
 				type="text" name="newnumberquantity"
 				value="">
 <label for="newnumberac">What area code do you need?</label>
-		 <input type="text" name="newnumberac" value="">				
-			</div>	
-<label for= "emergphonenumber">Which number will be your 911 number?<span class="required" style="color:red;">*</span></label>
+		 <input type="text" name="newnumberac" value="">					
+<label for= "emergnewnumber">Do you want one of the new numbers to be 911 provisioned?<span class="required" 
+style="color:red;">*</span></label>Yes<input type= "radio" name="emergnewnumber" id="yes-911new" value="yes">No<input type= "radio"
+name="emergnewnumber" id="no-911new" value="no">
+</div>	
 <br>
 <label for="virtualnumbers">Will you need any virtual numbers<span class="required" style="color:red;">*</span>
 </label> Yes<input type="radio" name="virtualnumbers" id="virtualnumbers-yes"
-				value="Yes"> No<input type="radio"
-				name="virtualnumbers" id="virtualnumbers-no" value="No">
+				value="2"> No<input type="radio"
+				name="virtualnumbers" id="virtualnumbers-no" value="1">
 				<br>
-				<div class="reveal-if-active">
+				<div class="yesvtn">
 <label for="vtnquantity"> How many numbers will you need?</label> <input
 				type="text" name="vtnquantity"
 				value="">
@@ -73,37 +72,65 @@ if ($OrderItemsResult->num_rows > 0) {
 				</div>
 </form>
 </div>
+
 <script>
 var intTextBox = 0;
 	function addElement() {
 		intTextBox++;
 		var objNewDiv = document.createElement('div');
 		objNewDiv.setAttribute('id', 'div_' + intTextBox);
-		objNewDiv.innerHTML = 'Phone Number ' + intTextBox + ': <input type= "text" id="tb_' + intTextBox + '" name="tb_' + intTextBox + '" />';
+		objNewDiv.innerHTML = 'Phone Number ' + intTextBox + ': <input type= "text" id="tb_' + intTextBox + '" name="tb_' + intTextBox + '" />'
+		+ '911?: <input type = "checkbox" id="911_' + intTextBox + '" name="911_' + intTextBox + '" />' + 'BTN?: <input type = "checkbox" id="BTN_' + intTextBox + '" name="BTN_' + intTextBox + '" />' ;
 		document.getElementById('content').appendChild(objNewDiv);
-	}
+	};
 	function removeElement() {
 	    if(0 < intTextBox) {
 	        document.getElementById('content').removeChild(document.getElementById('div_' + intTextBox));
 	        intTextBox--;
 	    } else {
 	        alert("No phone number to remove");
-	    }
+	    };
+	};
+	$('input[name="porting"]').on('change', function() {
+		$('.yesport')
+			.toggle(+this.value === 2 && this.checked);
+	}).change();
+	$('input[name="newnumbers"]').on('change', function() {
+		$('.yesnew')
+			.toggle(+this.value === 2 && this.checked);
+	}).change();
+	$('input[name="virtualnumbers"]').on('change', function() {
+		$('.yesvtn')
+			.toggle(+this.value === 2 && this.checked);
+	}).change();
+	//http://stackoverflow.com/questions/27546071/how-to-toggle-div-visibility-using-radio-buttons source for above snippet.
+	/*function toggle_visibility('reveal-if-active') {
+		var e = document.getElementById('reveal-if-active');
+		if(e.style.display == 'block')
+			e.style.display = 'none';
+		else
+			e.style.display = 'block';
 	}
+	onclick=toggle_visibility('yes');
+	*/
+	//http://stackoverflow.com/questions/16308779/how-can-i-hide-show-a-div-when-a-button-is-clicked source for above snippet
+	/*
 	$(document).ready(function() {
+		$('input[type="radio"]').click(function() {
+			if(($this).attr('id') == 'yes') {
+				$('#reveal-if-active').show();
+		}
+		else {
+			$('#reveal-if-active').hide();
+		}
+		});
+	});
+	*/
+	/* $(document).ready(function() {
 		$("#hide").click(function() {
 			$("p").hide();
 		});
 	});
-	function generate () {
-		var a = parseInt(document.getElementById("numberdetail").value);
-		var numbers = document.getElementById("ch");
-
-		for (i=0; i < a; i++) {
-			var input = document.createElement("input");
-			numbers.appendChild(input);
-		}
-	}
 	var FormStuff = {
 			  
 			  init: function() {
@@ -131,6 +158,7 @@ var intTextBox = 0;
 			};
 
 			FormStuff.init();
+			*/
 </script>
 </body>
 </html>
