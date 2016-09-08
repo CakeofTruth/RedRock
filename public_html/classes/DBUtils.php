59ccf064e9f50aef64a48595c3e87dd0a7acf0c7
Cake <jbowdish@email.arizona.edu>
2016-09-08 00:07:21 -0700

    Wiill no longer fail to place an order if there are no ported number details

--- public_html/classes/DBUtils.php
+++ public_html/classes/DBUtils.php
@@ -12,10 +12,10 @@ $dbname = "RedRock";
 */
 
 	function getDBConnection() {
-		$servername = "127.0.0.1:3307";
+		$servername = "127.0.0.1";
 		$dbname = "redroc91_redrock";
-        $dbusername = "root";
-		$dbpassword = "";
+        $dbusername = "redroc91_redrock";
+		$dbpassword = "@dm!nP@$$1001";
 
 		// Create connection
 		$conn = new mysqli ( $servername, $dbusername, $dbpassword, $dbname );
